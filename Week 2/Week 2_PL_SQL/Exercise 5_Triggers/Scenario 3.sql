84ea6dca312e07965cb3aca13f890e5086a7d409
Anish Das <anishservice12@gmail.com>
2024-08-05 17:43:08 +0530

    Added Exercise 6'(PL_SQL) for Week 2

--- Week 2/Week 2_PL_SQL/Exercise 5_Triggers/Scenario 3.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER LogTransaction
-AFTER INSERT ON Transactions
-FOR EACH ROW
-BEGIN
-    INSERT INTO AuditLog (AuditID, TransactionID, AccountID, TransactionDate, Amount, TransactionType, LogDate)
-    VALUES (AuditLog_seq.NEXTVAL, :NEW.TransactionID, :NEW.AccountID, :NEW.TransactionDate, :NEW.Amount, :NEW.TransactionType, SYSDATE);
-END LogTransaction;
-/
