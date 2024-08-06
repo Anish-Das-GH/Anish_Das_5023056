8b36a1af8f61908381ef473a7851ae8e3f63f4ca
Anish Das <anishservice12@gmail.com>
2024-08-06 14:28:15 +0530

    Added Exercise 7(Spring Maven) for Week 2

---
+++ Week 2/Week 2_Spring_Core_Maven/Exercise 7_Constructor_and_Setter _Injection/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    @Override
+    public String toString() {
+        return "BookRepository instance";
+    }
+}
