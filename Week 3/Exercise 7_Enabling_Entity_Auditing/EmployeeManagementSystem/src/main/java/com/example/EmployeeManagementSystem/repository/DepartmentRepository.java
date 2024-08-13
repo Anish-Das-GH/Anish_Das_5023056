51875dacb295d19d0aed009a15acdace674c1772
Anish Das <anishservice12@gmail.com>
2024-08-13 20:12:28 +0530

    Added Exercise 7 for Week 3

---
+++ Week 3/Exercise 7_Enabling_Entity_Auditing/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/DepartmentRepository.java
@@ -0,0 +1,17 @@
+package com.example.EmployeeManagementSystem.repository;
+
+import com.example.EmployeeManagementSystem.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Optional<Department> findByName(String name);
+
+    @Query("SELECT d FROM Department d WHERE d.name LIKE %?1%")
+    Optional<Department> findByNameContaining(String nameFragment);
+}
